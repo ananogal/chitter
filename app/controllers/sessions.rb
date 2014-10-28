fa306961cab0579a45c716984df242d88101837f
ananogal <ana.nogal@gmail.com>
2014-10-28 06:11:23 +0000

    changed way of getting user of peeps. Added some margin in flash notice and error

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -8,6 +8,7 @@ post '/sessions' do
 	@user = User.authenticate(params[:email], params[:password])
 	if @user
 		session[:user_id] = @user.id
+		session[:peep_id] = nil
 		redirect to '/'
 	else
 		flash[:errors] = ["The email or password is incorrect."]
